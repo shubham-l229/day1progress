88e65dd524225950efe2a7d3c41b9aeeeda1b8b4
shubham-l229 <dummy@gmail.com>
2026-01-29 12:29:59 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -0,0 +1,10 @@
+package com.edutech.progressive.config;
+
+import java.sql.SQLException;
+
+public class DatabaseConnectionManager {
+    // private static void loadProperties();
+    // public static Connection getConnection() throws SQLException{
+    //     return 
+    // }
+}
---
+++ server/src/main/java/com/edutech/progressive/entity/Accounts.java
@@ -0,0 +1,33 @@
+package com.edutech.progressive.entity;
+
+public class Accounts {
+    private int accountId;
+    private int customerId;
+    private double balance;
+    public Accounts() {
+    }
+    public Accounts(int accountId, int customerId, double balance) {
+        this.accountId = accountId;
+        this.customerId = customerId;
+        this.balance = balance;
+    }
+    public int getAccountId() {
+        return accountId;
+    }
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+    public int getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+}
