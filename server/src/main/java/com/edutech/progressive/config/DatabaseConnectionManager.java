615ea67d833f04a33bc9f921008d4a8b1785568d
shubham-l229 <146950304+shubham-l229@users.noreply.github.com>
2026-02-02 18:38:46 +0530

    Update DatabaseConnectionManager.java

--- server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -1,47 +1,54 @@
 package com.edutech.progressive.config;
 
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.io.InputStream;
-
-import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class DatabaseConnectionManager {
 
-    private static final Properties properties = new Properties();
+   private static Properties properties = new Properties();
+   private static void loadProperties(){
+      try(InputStream input = DatabaseConnectionManager.class.getClassLoader().getResourceAsStream("application.properties")) {
+         properties.load(input);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   public static Connection getConnection() throws SQLException{
+      loadProperties();  
+      String url = properties.getProperty("spring.datasource.url");
+      String username =  properties.getProperty("spring.datasource.username");
+      String password = properties.getProperty("spring.datasource.password");
 
-    static {
-        loadProperties();
-    }
-    private static void loadProperties(){
-        try(InputStream input = DatabaseConnectionManager.class.getClassLoader().getResourceAsStream("application.properties")) {
-            properties.load(input);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    // @Autowired
-
-    // private static Environment env;
-    // private static void loadProperties(){
-    //     String url = env.getProperty("spring.datasource.url");
-    //     String username = env.getProperty("spring.datasource.username");
-    //     String password = env.getProperty("spring.datasource.password");
-
-    //     properties.put("url", url);
-    //     properties.put("username", username);
-    //     properties.put("password", password);
-    // }
-    public static Connection getConnection() throws SQLException{
-        // String url = (String) properties.get("url");
-        // String username = (String) properties.get("username");
-        // String password = (String) properties.get("password");
-        return DriverManager.getConnection(properties.getProperty("db.url"),properties.getProperty("db.username"),properties.getProperty("db.password"));
-        // return DriverManager.getConnection(null, null, null);
-    }
+      return DriverManager.getConnection(url, username, password);
+   }
+   
 }
+
+
+
+// CREATE TABLE customers(
+//    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+//    name VARCHAR(255) NOT NULL,
+//    email VARCHAR(255) NOT NULL,
+//    username VARCHAR(255) NOT NULL,
+//    password VARCHAR(255) NOT NULL,
+//    role VARCHAR(255)
+// );
+
+// CREATE TABLE transactions (
+//    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+//    account_id INT NOT NULL,
+//    amount DECIMAL(10,2) NOT NULL,
+//    transaction_date TIMESTAMP NOT NULL,
+//    transaction_type VARCHAR(255) NOT NULL
+// );
+
+// CREATE TABLE accounts (
+//  account_id INT AUTO_INCREMENT PRIMARY KEY,
+//  customer_id INT NOT NULL,
+//  balance DECIMAL(10,2) NOT NULL
+// );
