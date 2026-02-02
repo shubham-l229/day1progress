dd60ab38346ade7457664d320db9c91237bcca6b
shubham-l229 <146950304+shubham-l229@users.noreply.github.com>
2026-02-02 18:46:39 +0530

    Update Transactions.java

--- server/src/main/java/com/edutech/progressive/entity/Transactions.java
+++ server/src/main/java/com/edutech/progressive/entity/Transactions.java
@@ -2,71 +2,56 @@ package com.edutech.progressive.entity;
 import java.util.Date;
 
 public class Transactions {
-    private int transactionId;
-    private int accountId;
-    private double amount;
-    private String transactionType; // DEPOSIT , WITHDRAWAL
-    private Date transactionDate;
-    public Transactions(){}
+  private int transactionId;
+  private int accountId;
+  private double amount;
+  private Date transactionDate;
+  private String transactionType;
+  
+
+  public Transactions(){
     
-
-    public Transactions(int transactionId, int accountId, double amount, String transactionType, Date transactionDate) {
-        this.transactionId = transactionId;
-        this.accountId = accountId;
-        this.amount = amount;
-        this.transactionType = transactionType;
-        this.transactionDate = transactionDate;
-    }
-
-
-    public int getTransactionId() {
-        return transactionId;
-    }
+  }
+  
+ 
+  public Transactions(int transactionId, int accountId, double amount, Date transactionDate, String transactionType) {
+    this.transactionId = transactionId;
+    this.accountId = accountId;
+    this.amount = amount;
+    this.transactionDate = transactionDate;
+    this.transactionType = transactionType;
+  }
 
 
-    public void setTransactionId(int transactionId) {
-        this.transactionId = transactionId;
-    }
+  public int getTransactionId() {
+    return transactionId;
+  }
+  public void setTransactionId(int transactionId) {
+    this.transactionId = transactionId;
+  }
+  public int getAccountId() {
+    return accountId;
+  }
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+  public double getAmount() {
+    return amount;
+  }
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
+  public String getTransactionType() {
+    return transactionType;
+  }
+  public void setTransactionType(String transactionType) {
+    this.transactionType = transactionType;
+  }
+  public Date getTransactionDate() {
+    return transactionDate;
+  }
+  public void setTransactionDate(Date transactionDate) {
+    this.transactionDate = transactionDate;
+  }
 
-
-    public int getAccountId() {
-        return accountId;
-    }
-
-
-    public void setAccountId(int accountId) {
-        this.accountId = accountId;
-    }
-
-
-    public double getAmount() {
-        return amount;
-    }
-
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-
-    public String getTransactionType() {
-        return transactionType;
-    }
-
-
-    public void setTransactionType(String transactionType) {
-        this.transactionType = transactionType;
-    }
-
-
-    public Date getTransactionDate() {
-        return transactionDate;
-    }
-
-
-    public void setTransactionDate(Date transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    
-} 
+}
