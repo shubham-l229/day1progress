9732adb612a15dc4e94769a91ffabdb630827b94
shubham-l229 <146950304+shubham-l229@users.noreply.github.com>
2026-02-02 18:40:18 +0530

    Update AccountDAOImpl.java

--- server/src/main/java/com/edutech/progressive/dao/AccountDAOImpl.java
+++ server/src/main/java/com/edutech/progressive/dao/AccountDAOImpl.java
@@ -1,35 +1,97 @@
 package com.edutech.progressive.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.edutech.progressive.config.DatabaseConnectionManager;
 import com.edutech.progressive.entity.Accounts;
+import com.edutech.progressive.service.AccountService;
 
 public class AccountDAOImpl implements AccountDAO{
-
-    @Override
-    public List<Accounts> getAllAccounts() {
-        return null;
-    }
-    public List<Accounts> getAllAccountsByCustomer(int customer_id){
-        return new ArrayList<>();
-    }
-    @Override
-    public Accounts getAccountById(int accountId) {
-        return null;
-    }
-    @Override
-    public int addAccount(Accounts accounts) {
-        return -1;
-    }
-    @Override
-    public void updateAccount(Accounts accounts) {
-        
-    }
-    @Override
-    public void deleteAccount(int accountId) {
-        
+   
+    private Connection connection;
+    public AccountDAOImpl(){
+        try {
+            connection = DatabaseConnectionManager.getConnection();
+        } catch (SQLException e) {
+             e.printStackTrace();
+        }
     }
 
+
+    @Override
+    public int addAccount(Accounts accounts) throws SQLException {
+        String sql="INSERT INTO accounts(customer_id,balance) VALUES(?,?)";
+        PreparedStatement ps = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, accounts.getCustomerId());
+            ps.setDouble(2, accounts.getBalance());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+            return -1;
+
+    }
+
+    @Override
+    public Accounts getAccountById(int accountId) throws SQLException {
+        String sql="SELECT * FROM accounts WHERE account_id=?";
+       PreparedStatement ps = connection.prepareStatement(sql);
+     
+            ps.setInt(1, accountId);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return new Accounts(rs.getInt("account_id"), rs.getInt("customer_id"), rs.getDouble("balance"));
+            }
+            return null;
+
+            
+       
+    }
+
+    @Override
+    public void updateAccount(Accounts accounts) throws SQLException {
+        String sql="UPDATE accounts SET  balance=? WHERE account_id=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setDouble(1, accounts.getBalance());
+            ps.setInt(2, accounts.getAccountId());
+            ps.executeUpdate();
+            
+      
+    }
+
+    @Override
+    public void deleteAccount(int accountId) throws SQLException {
+        String sql="DELETE FROM accounts WHERE account_id=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+             ps.setInt(1, accountId);
+              ps.executeUpdate();
+             
+
+      
+    }
+
+    @Override
+    public List<Accounts> getAllAccounts() throws SQLException {
+        List<Accounts> list = new ArrayList<>();
+        String sql="SELECT * FROM accounts";
+        PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+              Accounts acc =   new Accounts(rs.getInt("account_id"), rs.getInt("customer_id"), rs.getDouble("balance"));
+              list.add(acc);
+            }
+            return list;
+        
+    }
     
 }
+
+
+// ___________________________________________ NEW CHANGES DONE__________________________________________
