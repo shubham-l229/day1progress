ef584b74e571d98f17cd2e1d5ce759b114f91fe1
shubham-l229 <146950304+shubham-l229@users.noreply.github.com>
2026-02-02 18:50:50 +0530

    Update CustomerServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
@@ -2,28 +2,30 @@ package com.edutech.progressive.service.impl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.edutech.progressive.entity.Customers;
-import com.edutech.progressive.service.CustomerService;
 
-public class CustomerServiceImplArraylist implements CustomerService{ 
-    private static final List<Customers> customersList = new ArrayList<>();
-    public List<Customers> getAllCustomers()throws SQLException{
+public class CustomerServiceImplArraylist {
+
+    private static List<Customers> customersList = new ArrayList<>();
+
+    public List<Customers> getAllCustomers() throws SQLException {
         return customersList;
     }
-    public int addCustomer(Customers customers) throws SQLException{
+
+    public int addCustomer(Customers customers) throws SQLException {
         customersList.add(customers);
         return customersList.size();
     }
-    public List<Customers> getAllCustomersSortedByName()throws SQLException{
-        // List<Customers> sortedList = new ArrayList<>(customersList);
-        Collections.sort(customersList);
+
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
+        customersList.sort((a, b) ->
+                a.getName().compareToIgnoreCase(b.getName()));
         return customersList;
     }
-    public void emptyArrayList(){
-        // customersList = new ArrayList<>();
+
+    public void emptyArrayList() {
         customersList.clear();
     }
 }
