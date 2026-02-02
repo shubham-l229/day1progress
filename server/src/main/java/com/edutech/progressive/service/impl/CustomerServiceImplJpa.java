62c3799c42b6d9ed6b75cd10b894caa914ddb490
shubham-l229 <146950304+shubham-l229@users.noreply.github.com>
2026-02-02 18:51:44 +0530

    Update CustomerServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplJpa.java
@@ -1,5 +1,42 @@
 package com.edutech.progressive.service.impl;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.edutech.progressive.dao.CustomerDAO;
+import com.edutech.progressive.entity.Customers;
+
 public class CustomerServiceImplJpa {
-    
+
+    private CustomerDAO customerDAO;
+
+    public CustomerServiceImplJpa(CustomerDAO customerDAO) {
+        this.customerDAO = customerDAO;
+    }
+
+    public List<Customers> getAllCustomers() throws SQLException {
+        return customerDAO.getAllCustomers();
+    }
+
+    public Customers getCustomerById(int customerId) throws SQLException {
+        return customerDAO.getCustomerById(customerId);
+    }
+
+    public int addCustomer(Customers customers) throws SQLException {
+        return customerDAO.addCustomer(customers);
+    }
+
+    public void updateCustomer(Customers customers) throws SQLException {
+        customerDAO.updateCustomer(customers);
+    }
+
+    public void deleteCustomer(int customerId) throws SQLException {
+        customerDAO.deleteCustomer(customerId);
+    }
+
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
+        List<Customers> list = customerDAO.getAllCustomers();
+        list.sort((a, b) -> a.getName().compareToIgnoreCase(b.getName()));
+        return list;
+    }
 }
