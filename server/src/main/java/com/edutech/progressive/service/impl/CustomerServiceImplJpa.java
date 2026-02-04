327d97de827534bb89640ae59a7c8a4a848bb27e
shubham-l229 <146950304+shubham-l229@users.noreply.github.com>
2026-02-04 20:59:59 +0530

    Update CustomerServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplJpa.java
@@ -3,40 +3,47 @@ package com.edutech.progressive.service.impl;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.edutech.progressive.dao.CustomerDAO;
 import com.edutech.progressive.entity.Customers;
-
+import com.edutech.progressive.repository.CustomerRepository;
+import com.edutech.progressive.service.CustomerService;
+@Service
 public class CustomerServiceImplJpa {
+    
+    // @Autowired
+    private CustomerRepository custRepo;
 
-    private CustomerDAO customerDAO;
-
-    public CustomerServiceImplJpa(CustomerDAO customerDAO) {
-        this.customerDAO = customerDAO;
+    public CustomerServiceImplJpa(CustomerRepository custRepo) {
+        this.custRepo = custRepo;
     }
 
-    public List<Customers> getAllCustomers() throws SQLException {
-        return customerDAO.getAllCustomers();
+    public List<Customers>  getAllCustomers(){
+        return custRepo.findAll();
     }
 
-    public Customers getCustomerById(int customerId) throws SQLException {
-        return customerDAO.getCustomerById(customerId);
+    public Customers getCustomerById(int customerId){
+        return custRepo.findByCustomerId(customerId);
     }
 
-    public int addCustomer(Customers customers) throws SQLException {
-        return customerDAO.addCustomer(customers);
+    public int addCustomer(Customers customers){
+        return custRepo.save(customers).getCustomerId();
     }
 
-    public void updateCustomer(Customers customers) throws SQLException {
-        customerDAO.updateCustomer(customers);
+    public void updateCustomer(Customers customers){
+        custRepo.save(customers);
     }
 
-    public void deleteCustomer(int customerId) throws SQLException {
-        customerDAO.deleteCustomer(customerId);
+    public void deleteCustomer(int customerId){
+        custRepo.deleteByCustomerId(customerId);
     }
 
-    public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        List<Customers> list = customerDAO.getAllCustomers();
-        list.sort((a, b) -> a.getName().compareToIgnoreCase(b.getName()));
-        return list;
+    public List<Customers> getAllCustomersSortedByName(){
+       List<Customers> list = custRepo.findAll();
+       list.sort(Customers::compareTo);
+       return list;
     }
+    
 }
